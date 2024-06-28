26b54c4b3848e5a7879ba203005abd7c425ad400
Morgan Etheridge <kemmelingm@gmail.com>
2024-06-28 11:58:24 -0400

    Added SQL Query

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,8 @@
--- Add your sql here
+SELECT customers.customer_name, 
+       SUM(subscriptions.price_per_month * subscriptions.subscription_length) AS amount_due
+FROM customers
+JOIN orders ON customers.customer_id = orders.customer_id
+JOIN subscriptions ON orders.subscription_id = subscriptions.subscription_id
+WHERE orders.order_status = 'unpaid' AND subscriptions.description = 'Fashion Magazine'
+GROUP BY customers.customer_name;
+
